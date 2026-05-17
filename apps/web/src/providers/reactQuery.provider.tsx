e44ad9543de6d9ac6cdc0e1a0ccedd4e0ac6b818
Tauseef-Hilal <tantary.tauseef.atwork@gmail.com>
2026-05-17 20:01:20 +0530

    fix: memory leaks in web service

--- apps/web/src/providers/reactQuery.provider.tsx
+++ apps/web/src/providers/reactQuery.provider.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
-
 export function ReactQueryProvider({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
